6cf5440122a200a0231ed56105d024d0b04759d8
Alex Cheung <alexkk.cheung@gmail.com>
2022-09-08 16:48:04 +0800

    first commit
    - use package gorilla websocket
    - use uikit css framework

---
+++ my-readme.md
@@ -0,0 +1,3 @@
+2022-9-8
+
+https://blog.logrocket.com/using-websockets-go/
---
+++ server.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+var todoList []string
+var completeTodoList []string
+
+func getCmd(input string) string {
+	inputArr := strings.Split(input, " ")
+
+	return inputArr[0]
+}
+
+func getMessage(input string) string {
+	inputArr := strings.Split(input, " ")
+	var result string
+	for i := 1; i < len(inputArr); i++ {
+		result += inputArr[i]
+	}
+
+	return result
+}
+
+func updateTodoList(input string) {
+	tmpList := todoList
+	todoList = []string{}
+
+	//tmpCompleteList := completeTodoList
+	//completeTodoList = []string{}
+
+	for _, val := range tmpList {
+		if val == input {
+			completeTodoList = append(completeTodoList, val)
+			continue
+		}
+
+		todoList = append(todoList, val)
+	}
+}
+
+func main() {
+	//fmt.Println("vim-go")
+
+	http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("upgrade failed: ", err)
+			return
+		}
+		defer conn.Close()
+
+		for {
+			mt, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read failed:", err)
+				break
+			}
+
+			input := string(message)
+			cmd := getCmd(input)
+			msg := getMessage(input)
+
+			if cmd == "add" {
+				todoList = append(todoList, msg)
+			} else if cmd == "done" {
+				updateTodoList(msg)
+			}
+
+			//output := "Current Todos: \n"
+			output := ""
+
+			if len(todoList) > 0 {
+				output += "<span uk-icon='icon: check'></span>Current Todos:"
+			}
+
+			output += "<ol>"
+
+			for _, todo := range todoList {
+				//output += "\n - " + todo + "\n"
+				output += "<li><input type='checkbox' value='" + todo + "' onclick='handleClick(this);'>" + todo + "</li>"
+			}
+
+			//output += "\n--------------------------------------"
+			output += "</ol>"
+
+			if len(completeTodoList) > 0 {
+				output += "<h2>Complete Todos:</h2>"
+			}
+
+			output += "<ul>"
+
+			for _, todo := range completeTodoList {
+				//output += "\n - " + todo + "\n"
+				output += "<li><s>" + todo + "</s></li>"
+			}
+
+			output += "</ul>"
+
+			message = []byte(output)
+			err = conn.WriteMessage(mt, message)
+
+			if err != nil {
+				log.Println("write failed:", err)
+				break
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Fprintf(w, "Setting up the server!")
+		http.ServeFile(w, r, "websockets.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
